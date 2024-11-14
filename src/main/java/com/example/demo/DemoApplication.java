26a9a4f18d39a9da20fc28ca2247a6a36d56255d
Sourav Kalra <Sourav.Kalra@watchguard.com>
2024-11-15 00:30:35 +0530

    initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
